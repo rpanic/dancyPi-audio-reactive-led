46a0fcebacc705a5f9e7985d9d7b2aecfb88101e
rpanic <raphael.panic@gmx.at>
2020-08-12 22:28:19 +0200

    logging

--- python/web.py
+++ python/web.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route('/start/<effect>')
 def start(effect):
+    print("Starting " + effect)
     setEffect(effect)
     if('stream' not in globals() or stream == None):
+        print("First one, starting audio")
         visualization_start()
 
 @app.route('/stop')
 def stop():
+    print("Stopping audio")
     stream.stop_stream()
     stream.close()
 
